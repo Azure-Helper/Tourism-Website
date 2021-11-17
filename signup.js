67a31e9ad17a76912b538c1a53a38bc5ba14a2e6
Azure-Helper <azurehelper219@gmail.com>
2021-11-17 05:34:39 +0530

    Validation

--- signup.js
+++ signup.js
@@ -1,80 +1,78 @@
+let uname = document.getElementById('uname');
+let email = document.getElementById('email');
+let phone1 = document.getElementById('phone1');
+let password1 = document.getElementById('password1');
+let password2 = document.getElementById('password2');
+
 function validate(){
-    var uname = document.getElementById('uname').value;
-    var email = document.getElementById('email').value;
-    var password1 = document.getElementById('password1').value;
-    var password2 = document.getElementById('password2').value;
-    var phone1 = document.getElementById('phone1').value;
-    if(uname == ""){
-        document.getElementById("usernamee").innerHTML ="Username Field Cannot Be Empty";
+    if(uname.value == ""){
+        uname.style.border="2px solid red";
+        document.getElementById("username1").innerHTML ="Username* Field Cannot Be Empty";
         return false; 
     }
-    if((uname.length <= 2) || (uname.length > 20)) { 
-        document.getElementById("usernamee").innerHTML ="Username Length Must be between 2 and 20";
-         return false; 
+    if((uname.value.length <= 2) || (uname.value.length > 20)) {
+        uname.style.border="2px solid red"; 
+        document.getElementById("username1").innerHTML ="Username* Length Must be between 2 and 20";
+        return false; 
      }
-     if(!isNaN(uname)){
-        document.getElementById('usernamee').innerHTML =" ** only characters are allowed";
-        return false;
+     if(!isNaN(uname.value)){
+         uname.style.border="2px solid red";
+         document.getElementById('username1').innerHTML ="Username* Only Characters are allowed";
+         return false;
     }
 
-
-
-
-    if(email == ""){
-        document.getElementById('emaill').innerHTML =" ** Please fill the email id` field";
+    if(email.value == ""){
+        email.style.border="2px solid red";
+        document.getElementById('email1').innerHTML ="Email* Field Cannot Be Empty";
         return false;
     }
-    if(email.indexOf('@') <= 0 ){
-        document.getElementById('emaill').innerHTML =" ** @ Invalid Position";
+    if(email.value.indexOf('@') <= 0 ){
+        email.style.border="2px solid red";
+        document.getElementById('email1').innerHTML ="Email* @ Invalid Position";
         return false ;
     }
-
-    if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
-        document.getElementById('emaill').innerHTML =" ** . Invalid Position";
+    if((email.value.charAt(email.value.length-4)!='.') && (email.value.charAt(email.value.length-3)!='.')){
+        email.style.border="2px solid red";
+        document.getElementById('email1').innerHTML ="Email* . Invalid Position";
         return false;
     }
 
-
-
-
-
-    if(password1 == ""){
-        document.getElementById('pass').innerHTML =" ** Please fill the password field";
+    if(phone1.value == ""){
+        phone1.style.border="2px solid red";
+        document.getElementById('phoneno').innerHTML ="Phone* Field Cannot Be Empty";
         return false;
     }
-    if((password1.length <= 5) || (password1.length > 20)) {
-        document.getElementById('pass').innerHTML =" ** Passwords lenght must be between  5 and 20";
+    if(isNaN(phone1.value)){
+        phone1.style.border="2px solid red";
+        document.getElementById('phoneno').innerHTML ="Phone* Only Digits are allowed";
+        return false;
+    }
+    if(phone1.value.length!=10){
+        phone1.style.border="2px solid red";
+        document.getElementById('phoneno').innerHTML ="Phone* Length Must be 10 digits only";
+        return false;
+    }
+
+    if(password1.value == ""){
+        password1.style.border="2px solid red";
+        document.getElementById('pass1').innerHTML ="Password* Field Cannot Be Empty";
+        return false;
+    }
+    if((password1.value.length <= 5) || (password1.value.length > 20)) {
+        password1.style.border="2px solid red";
+        document.getElementById('pass1').innerHTML ="Password* Length Must be between 5 and 20";
         return false;	
     }
 
-
-    if(password1!=password2){
-        document.getElementById('pass1').innerHTML =" ** Password does not match the confirm password";
+    if(password1.value!=password2.value){
+        password2.style.border="2px solid red";
+        document.getElementById('pass2').innerHTML ="Confirm Password* Does not match";
         return false;
     }
-
-
-
-    if(password2 == ""){
-        document.getElementById('pass1').innerHTML =" ** Please fill the confirmpassword field";
+    if(password2.value == ""){
+        password2.style.border="2px solid red";
+        document.getElementById('pass2').innerHTML ="Confirm Password* Field Cannot Be Empty";
         return false;
     }
-
-
-    if(phone1 == ""){
-        document.getElementById('mobileno').innerHTML =" ** Please fill the mobile NUmber field";
-        return false;
-    }
-    if(isNaN(phone1)){
-        document.getElementById('mobileno').innerHTML =" ** user must write digits only not characters";
-        return false;
-    }
-    if(phone1.length!=10){
-        document.getElementById('mobileno').innerHTML =" ** Mobile Number must be 10 digits only";
-        return false;
-    }
-
-
-
 
 }
